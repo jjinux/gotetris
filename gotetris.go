3444ca088859564e5098f62f493a4b12b1dced3b
Shannon -jj Behrens <sbehrens@twitter.com>
2015-05-20 19:19:08 -0700

    Finished the UI

--- gotetris.go
+++ gotetris.go
@@ -9,6 +9,8 @@ just a simple video game ;)
 package main
 
 import (
+	"fmt"
+	"strings"
 	"time"
 
 	"github.com/nsf/termbox-go"
@@ -16,20 +18,37 @@ import (
 
 const backgroundColor = termbox.ColorBlack
 const instructionsColor = termbox.ColorWhite
+
 const defaultMarginWidth = 2
 const defaultMarginHeight = 1
+const titleStartX = defaultMarginWidth
+const titleStartY = defaultMarginHeight
+const titleHeight = 1
+const titleEndY = titleStartY + titleHeight
 const boardStartX = defaultMarginWidth
-const boardStartY = defaultMarginHeight
+const boardStartY = titleEndY + defaultMarginHeight
 const boardWidth = 10
 const boardHeight = 16
 const boardEndX = boardStartX + boardWidth
 const boardEndY = boardStartY + boardHeight
 const instructionsStartX = boardEndX + defaultMarginWidth
-const instructionsStartY = defaultMarginHeight
+const instructionsStartY = boardStartY
+
+const title = "Tetris Written in Go"
 
 var instructions = []string{
-	"Use arrow keys",
-	"Press down to make the piece fall",
+	"Goal: Fill in 5 lines!",
+	"",
+	"\u2190      Left",
+	"\u2192      Right",
+	"\u2191      Rotate",
+	"\u2193      Drop faster",
+	"s      Start",
+	"p      Pause",
+	"esc    Exit",
+	"",
+	"Level: %v",
+	"Lines: %v",
 }
 
 func tbprint(x, y int, fg, bg termbox.Attribute, msg string) {
@@ -41,12 +60,18 @@ func tbprint(x, y int, fg, bg termbox.Attribute, msg string) {
 
 func draw() {
 	termbox.Clear(backgroundColor, backgroundColor)
+	tbprint(titleStartX, titleStartY, instructionsColor, backgroundColor, title)
 	for y := boardStartY; y < boardEndY; y++ {
 		for x := boardStartX; x < boardEndX; x++ {
 			termbox.SetCell(x, y, ' ', termbox.ColorGreen, termbox.ColorGreen)
 		}
 	}
 	for i, instruction := range instructions {
+		if strings.HasPrefix(instruction, "Level:") {
+			instruction = fmt.Sprintf(instruction, 0)
+		} else if strings.HasPrefix(instruction, "Lines:") {
+			instruction = fmt.Sprintf(instruction, 0)
+		}
 		tbprint(instructionsStartX, instructionsStartY+i, instructionsColor, backgroundColor, instruction)
 	}
 	termbox.Flush()
